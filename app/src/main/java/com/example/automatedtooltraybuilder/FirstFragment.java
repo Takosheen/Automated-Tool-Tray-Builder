23a1dee04c1f671c1ac03a2c8c38532b27d24e44
Terence Au <60273779+Takosheen@users.noreply.github.com>
2021-03-18 20:19:47 -0400

    3/18/21 Meeting - Changed heading and formatting of the main menu, removed most extraneous features, and defined what the menu would look like.

---
+++ app/src/main/java/com/example/automatedtooltraybuilder/FirstFragment.java
@@ -0,0 +1,103 @@
+package com.example.automatedtooltraybuilder;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+
+
+
+
+/*
+//CAPTURE IMAGE CODE WIP, Based off of android example code atm-----------------------------
+static final int REQUEST_IMAGE_CAPTURE=1;
+        string currentPhotoPath;
+
+private void dispatchTakePictureIntent(){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try{
+        startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+        catch(ActivityNotFoundException e){
+        System.out.println("An error has occured, image capture failed.")
+        }
+        }
+
+//Creates image as a JPG, with date and path.
+private File createImageFile()throws IOException{
+        //File Image Created
+        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName="JPEG_"+timestamp+"_";
+        File storageDir=getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image=File.createTempFile(
+        imageFileName,
+        ".jpg",
+        storageDir);
+
+        currentPhotoPath=image.getAbsolutePath();
+        return image;
+
+        }
+
+private void dispatchTakePictureIntent(){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null)
+        {
+        File photoFile=null;
+        try
+        {
+        photoFile=createImageFile();
+        }
+        catch(IOException excep)
+        {
+        System.out.println("An error has occurred")
+        }
+        if (photoFile != null)
+            {
+        Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider",
+                                                            photoFile);
+          takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+          startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+
+            }
+          }
+        }
+      }
+
+
+
+
+//END OF CAPTURE IMAGE CODE------------------------------------------
+
+*/
+
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        /*view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });*/
+    }
+}
---
+++ app/src/main/java/com/example/automatedtooltraybuilder/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.automatedtooltraybuilder;
+/*
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+//this method turns it into a vectorized file, maybe use a height mapper?, will need to detect objects in an image (Rubens J)
+public class MainActivity{
+  try{
+    
+    
+  }
+  catch(IOException){
+    System.out.println("No image available.");
+  }
+}
+
+
+
+//method to take the image and turn it into a vectorized file -> in FirstFragment.java
+
+//method to automatically optimize the vectorized file after taking the picture -> in SecondFragment.java
+
+
+//testing testing
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}*/
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Define the pic id
+    private static final int pic_id = 123;
+
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // By ID we can get each component
+        // which id is assigned in XML file
+        // get Buttons and imageview.
+        camera_open_id = (Button)findViewById(R.id.CameraButton);
+        click_image_id = (ImageView)findViewById(R.id.click_image);
+
+        // Camera_open button is for open the camera
+        // and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+
+            // BitMap is data structure of image file
+            // which stor the image in memory
+            Bitmap photo = (Bitmap)data.getExtras()
+                    .get("data");
+
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+}
+
+    
